f454cc033298a8f83fc07d98407df8fad6db20ab
JulianaMancera <mancerajuliana98@gmail.com>
2025-12-15 08:41:53 +0800

    test

---
+++ Dart/Variables/var.dart
@@ -0,0 +1,21 @@
+void main() {
+ //Strings
+ //var name = "Luis";
+ String name = "Luis";
+  print(name);
+
+  //Integer
+  //int x = 41;
+  var x = 41;
+  print(x);
+
+  //Dynamic
+  dynamic firstName = "Lance";
+  print(firstName);
+
+  //Const and Final
+  const String fullName = "Lance Armstrong";
+  final String nickName = "Ace";
+  print(fullName);
+  print(nickName);
+}
