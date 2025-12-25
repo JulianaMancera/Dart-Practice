38e654ada85202c6dcc8d90722a7b9c471e16dc8
JulianaMancera <mancerajuliana98@gmail.com>
2025-12-25 20:05:05 +0800

    dt

---
+++ Dart/DataTypes/dt.dart
@@ -0,0 +1,24 @@
+void main(){
+  // Data Types
+  // Numbers, Strings, Booleans, Lists, Maps, Dynamic
+
+  //String 
+  String firstName = "Mark";
+  print("String: $firstName");
+
+  //Integer
+  int age = 30;
+  print("Integer: $age");
+
+  //Double
+  double otherNum = 19.95;
+  print("Double: $otherNum");
+
+  //Boolean
+  bool myBool = false;
+  print("Boolean: $myBool");
+
+  //Dynamic
+  dynamic someVar = "Hello mina-san";
+  print("Dynamic before: $someVar");
+}
